46effefbceb40a87102595e5414f45f4446ee678
Vincent Barrientos <113892354+Babinsss@users.noreply.github.com>
2026-07-12 02:16:11 +0800

    Refactor MySQL connection options to ensure system CA bundle is used for SSL

--- config/database.php
+++ config/database.php
@@ -58,9 +58,10 @@ return [
             'prefix_indexes' => true,
             'strict' => true,
             'engine' => null,
-            'options' => extension_loaded('pdo_mysql') ? array_filter([
-                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA', null),
-            ],) : [],
+            'options' => [
+            // Ensure this is set to use the system CA bundle or null
+             PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA', null),
+            ],
         ],
 
         'pgsql' => [
